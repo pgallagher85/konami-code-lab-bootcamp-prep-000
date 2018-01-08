e2e57a4c33d9f9337342ebc14db087ab74a4003f
Patrick Gallagher <pgallagher85@gmail.com>
2018-01-08 00:41:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,26 +3,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
 function init() {
-  document.body.addEventListener('keydown', function(e)) {
-    if (e.which === code) {
-      alert(`You've entered the correct code!`)
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which);
+    if (key === code[i]) {
+      i++;
+      if (i = code.length) {
+        alert(`You've entered the correct code!`);
+        i = 0;
+      }
     }
-  }
-  return null;
-}
-
-
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
-
-  if (key === code[index]) {
-    index++;
-    if (index === code.length) {
-      alert(`You've entered the correct code!`);
-      index = 0;
-    }
-  }
-  else {
     index = 0;
-  }
+  })
 }
+
