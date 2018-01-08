13a8655ddf3a506d3ccb47b059af9754fd8b41c6
Patrick Gallagher <pgallagher85@gmail.com>
2018-01-08 00:46:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-let i = 0;
+let index = 0;
 
 function init() {
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.which);
-    if (key === code[i]) {
-      i++;
+    if (key === code[index]) {
+      index++;
       if (i = code.length) {
         alert(`You've entered the correct code!`);
         i = 0;
